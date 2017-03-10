d9ab87243c71e330f95a377304dd0a78b74525b5
Hiren Joshi <hirenj@gmail.com>
2017-03-11 00:05:17 +0100

    Basic neighbouring gene annotation supplement

---
+++ js/genefilter.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const stream = require('stream');
+const util = require('util');
+const Transform = stream.Transform;
+
+function GeneFilter(gene_stream,mappings) {
+  if (!(this instanceof GeneFilter)) {
+    return new GeneFilter(gene_stream);
+  }
+
+  Transform.call(this, {objectMode: true});
+  this.stream = gene_stream;
+  this.mappings = mappings;
+  this.stream.on('end', () => delete this.stream );
+}
+
+util.inherits(GeneFilter, Transform);
+
+GeneFilter.prototype._transform = function (obj,enc,cb) {
+  obj.CHR_ID = parseInt(obj.CHR_ID);
+  obj.CHR_POS = parseInt(obj.CHR_POS);
+  if ( ! obj.CHR_POS || ! obj.CHR_ID ) {
+    // FIXME We need to sort out CHR x positions and sorting..
+    cb();
+    return;
+  }
+  if (obj.INTERGENIC !== '1') {
+    obj.UPSTREAM_PROTEIN_ENCODING_GENE_ID = null;
+    obj.DOWNSTREAM_PROTEIN_ENCODING_GENE_ID = null;
+    obj.UPSTREAM_PROTEIN_ENCODING_GENE_DISTANCE = null;
+    obj.DOWNSTREAM_PROTEIN_ENCODING_GENE_DISTANCE = null;
+    this.push(obj);
+    cb();
+    return;
+  }
+
+  if ( this.stream && ( ! this.first_gene || 
+                        this.first_gene.chromosome < obj.CHR_ID || 
+                        ( this.first_gene.chromosome == obj.CHR_ID && this.first_gene.start < obj.CHR_POS)
+                      ) ) {
+    if (this.first_gene) {
+      this.first_gene = null;
+    }
+    this.stream.on('data',(gene) => {
+      gene.chromosome = parseInt(gene.chromosome);
+      gene.start = parseInt(gene.start);
+      gene.geneid = this.mappings[gene.geneid];
+
+      this.stream.pause();
+      if (gene.chromosome < obj.CHR_ID ||
+          (gene.chromosome == obj.CHR_ID && gene.start < obj.CHR_POS ) ) {
+        this.prev_gene = gene;
+        this.stream.resume();
+      } else {
+        console.log("Previous gene before this position is ",this.prev_gene.geneid,this.prev_gene.chromosome,this.prev_gene.start,this.prev_gene.end);
+        console.log("First gene after this position is ",gene.geneid,gene.chromosome,gene.start);
+        console.dir(obj);
+        this.first_gene = gene;
+
+        obj.UPSTREAM_PROTEIN_ENCODING_GENE_ID = this.prev_gene.geneid;
+        obj.DOWNSTREAM_PROTEIN_ENCODING_GENE_ID = this.first_gene.geneid;
+        obj.UPSTREAM_PROTEIN_ENCODING_GENE_DISTANCE = obj.CHR_POS - this.prev_gene.end;
+        obj.DOWNSTREAM_PROTEIN_ENCODING_GENE_DISTANCE = this.first_gene.start - obj.CHR_POS;
+
+        this.stream.removeAllListeners('data');
+        this.stream.removeListener('end',this.stream.end_cb);
+        cb();
+      }
+    });
+    this.stream.end_cb = cb;
+    this.stream.on('end',this.stream.end_cb);
+    this.stream.resume();
+  } else {
+    // The first gene is after this gene.
+    obj.UPSTREAM_PROTEIN_ENCODING_GENE_ID = this.prev_gene.geneid;
+    obj.DOWNSTREAM_PROTEIN_ENCODING_GENE_ID = this.first_gene.geneid;
+    obj.UPSTREAM_PROTEIN_ENCODING_GENE_DISTANCE = obj.CHR_POS - this.prev_gene.end;
+    obj.DOWNSTREAM_PROTEIN_ENCODING_GENE_DISTANCE = this.first_gene.start - obj.CHR_POS;
+
+    this.push(obj);
+    cb();
+  }
+};
+
+GeneFilter.prototype._flush = function(cb) {
+  console.log("Completed GeneFilter");
+  cb();
+};
+
+function GeneMapping() {
+  if (!(this instanceof GeneMapping)) {
+    return new GeneMapping(gene_stream);
+  }
+
+  Transform.call(this, {objectMode: true});
+  this.data = {};
+  this.counter = 0;
+}
+
+util.inherits(GeneMapping, Transform);
+
+GeneMapping.prototype._transform = function (obj,enc,cb) {
+  this.counter++;
+  if (obj['#tax_id'] == '9606' && obj['Ensembl_protein_identifier'] !== '-') {
+    this.data[obj.Ensembl_gene_identifier] = obj.GeneID;
+  }
+  cb();
+};
+
+exports.GeneFilter = GeneFilter;
+exports.GeneMapping = GeneMapping;
---
+++ js/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require('fs');
+const csv = require('csv-parse');
+const GeneFilter = require('./genefilter').GeneFilter;
+const GeneMapping = require('./genefilter').GeneMapping;
+
+
+let gene_translation = new Promise((resolve,reject) => {
+  let csv_parser = csv({columns: true, delimiter: '\t'});
+  let mappings = fs.createReadStream('gene2ensembl');
+  let mapper = new GeneMapping();
+  let out_stream = mappings.pipe(csv_parser).pipe(mapper);
+  mapper.on('finish', () => {
+    resolve(mapper.data);
+  });
+  mapper.on('error', reject);
+});
+
+gene_translation.then( mappings => {
+  let positions_stream = fs.createReadStream('gene_positions.tsv').pipe(csv({columns:true,delimiter:"\t"}));
+  return new GeneFilter(positions_stream,mappings);
+}).then( filter => {
+  let catalog_stream = fs.createReadStream('sorted_gwas.tsv');
+  return catalog_stream.pipe(csv({columns: true, delimiter: '\t', relax: true})).pipe(filter);
+}).then( output => {
+  output.on('data', dat => console.log(dat) );
+}).catch( err => console.log(err));
